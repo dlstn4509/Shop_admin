8560166acc3366539d0cf076af2c2e7544d14c8f
dlstn4509 <dlstn7609@hanmail.net>
2021-10-28 12:54:07 +0900

    board list 완

--- routes/admin/user-router.js
+++ routes/admin/user-router.js
@@ -1,17 +1,21 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
-const { error } = require('../../modules/util');
+const { error, telNumber } = require('../../modules/util');
 
+// 회원 리스트
 router.get('/', (req, res, next) => {
   res.render('admin/user/user-list', { css: 'admin-user' });
 });
-router.post('/', (req, res, next) => {
-  res.send('admin/user:POST');
+// 회원 수정 화면
+router.get('/:id', (req, res, next) => {
+  res.render('admin/user/user-update', { css: 'admin-user', telNumber });
 });
+// 회원 수정
 router.put('/', (req, res, next) => {
   res.send('admin/user:PUT');
 });
+// 회원 삭제
 router.delete('/', (req, res, next) => {
   res.send('admin/user:DELETE');
 });
